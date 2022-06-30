bb40c89cba867bc4d40151e8bede8027efac5c7e
Irinaka Robdone <irinaka.robodone@gmail.com>
2022-06-30 20:13:49 +0900

    init repo of ki

--- src/snake_game.py
+++ src/snake_game.py
@@ -1,23 +1,21 @@
 from microbit import *
 import music
-
-current_pos = [0,0]
-
+x = 0
+y = 0
 while True:
-  current_pos = [0,0]           # スネークの頭の位置をリスト [x,y] で記憶する
-  display.clear()
-  while current_pos != [4,4]:   # スネークの最新の頭の位置が[4,4]になるまで以下の処理を繰り返す
-    if button_a.was_pressed():
-      current_pos[0] += 1
-    if button_b.was_pressed():
-      current_pos[1] += 1
+  
+  if x == 4 and y == 4:
+    music.play(music.POWER_UP)
+    x = 0
+    y = 0
+    display.clear()
+  if button_a.was_pressed():
+    x += 1
+    if x > 4:
+      x = 4
       
-    if current_pos[0] >= 4:     # x が 4 以上なら、これ以上進まないようにする
-      current_pos[0] = 4
-    if current_pos[1] >= 4:     # y が 4 以上なら、これ以上進まないようにする
-      current_pos[1] = 4
-      
-    display.set_pixel(current_pos[0], current_pos[1], 9)  # スネークの新しい頭の位置[x,y]を描画する
-    
-  music.play(music.POWER_UP)    # スネークの最新の頭の位置が [4,4] になった while ループを抜けて音を再生する
-    
+  elif button_b.was_pressed():
+    y += 1
+    if y > 4:
+      y = 4
+  display.set_pixel(x,y,9)
